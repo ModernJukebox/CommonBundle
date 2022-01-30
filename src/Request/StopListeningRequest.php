f724c5c0b4c42f630295556099dda8f62e75fdd8
chapterjason <jason@sourecode.dev>
2022-01-30 04:22:25 +0100

    feat: Add state to stop requests

--- src/Request/StopListeningRequest.php
+++ src/Request/StopListeningRequest.php
@@ -2,6 +2,19 @@
 
 namespace ModernJukebox\Bundle\Common\Request;
 
+use ModernJukebox\Bundle\Common\Enums\StationClientState;
+
 class StopListeningRequest
 {
+    private StationClientState $state;
+
+    public function __construct(StationClientState $state = StationClientState::IDLE)
+    {
+        $this->state = $state;
+    }
+
+    public function getState(): StationClientState
+    {
+        return $this->state;
+    }
 }
--- src/Request/StopServingRequest.php
+++ src/Request/StopServingRequest.php
@@ -2,6 +2,19 @@
 
 namespace ModernJukebox\Bundle\Common\Request;
 
+use ModernJukebox\Bundle\Common\Enums\StationServerState;
+
 class StopServingRequest
 {
+    private StationServerState $state;
+
+    public function __construct(StationServerState $state = StationServerState::IDLE)
+    {
+        $this->state = $state;
+    }
+
+    public function getState(): StationServerState
+    {
+        return $this->state;
+    }
 }
